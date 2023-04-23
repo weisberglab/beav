56ae0b6bcedf2fca44df66d7e117ba8409ee71a8
Alexandra Weisberg <weisbeal@onid.oregonstate.edu>
2023-04-23 09:24:42 -0700

    updated db download

--- scripts/setup_databases.sh
+++ scripts/setup_databases.sh
@@ -3,20 +3,34 @@ echo -e "downloading and setting up databases for dependencies"
 
 echo -e "BAKTA"
 
-baktadbpath="$1"
-baktadbtype="$2"
+baktadbtype="$1"
+baktadbpath="$2"
 
 #check if bakta db variable exists and db is downloaded
 if [[ ! -z "$BAKTA_DB" ]] && [[ -f $BAKTA_DB/version.json ]]; then
-	echo -e "BAKTA_DB already set and installed"
+	echo -e "BAKTA_DB already set and database is installed"
 else
-	if [[ ! -z $baktadbpath ]]; then
-		echo -e "Please ensure that the first argument to this script is the path to store the bakta db"
+	#if they only provide a type, install to baktafolder/db
+	#otherwise install to where they want it and remind to set BAKTA_DB
+	if [[ ! ($baktadbtype == "full" || $baktadbtype == "light") ]]; then
+		echo -e "Please ensure that the second argument to this script is either full (60GB database) or light (~3GB database)"
 		exit 1
-	elif [[ ! ($baktadbtype == "full" || $baktadbtype == "light") ]]; then
-		echo -e "Please ensure that the second argument to this script is either full or light"
+	if [[ -z $baktadbpath ]] && [[ ! -z $CONDA_PREFIX  ]]; then
+		echo -e "No bakta db path provided, defaulting to <baktadir>/db"
+		baktadbpath="$CONDA_PREFIX/db/"
+		if [[ ! -d $baktadbpath ]]; then
+			mkdir $baktadbpath
+		fi
+		echo -e "no need to set the BAKTA_DB environment variable."
+	else
+		echo -e "no bakta db path provided and not in a conda environment."
+		echo -e "Please activate the conda environment that beav is installed in and try again,"
+		echo -e "or provide a path to install the bakta_db."
+		echo -e "Dont forget to set the BAKTA_DB environment variable to point to that folder"
+		echo -e "alternatively, provide --db <yourbaktadbpath> as an argument to bakta"
 		exit 1
 	fi
+	
 	bakta_db download --output $baktadbpath --type $baktadbtype
 fi
 
@@ -29,17 +43,14 @@ echo -e ""
 
 #defense_finder
 echo -e "DEFENSE FINDER"
-
 defense-finder update
-
-
 echo -e ""
+
 echo -e "ANTISMASH"
-echo -e "not for now"
-#download-antismash-databases
-
+echo -e "(This may take a while, estimated 9GB download)"
+download-antismash-databases
 echo -e ""
-#TIGER2
+
 echo -e "TIGER2"
 if [[ ! -f $BEAV_DIR/software/TIGER/db/Pfam-A.hmm ]]; then
 	echo -e "requirement Pfam-A.hmm file does not exist"
@@ -47,23 +58,21 @@ if [[ ! -f $BEAV_DIR/software/TIGER/db/Pfam-A.hmm ]]; then
 	curl -L -O https://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz --output $BEAV_DIR/software/TIGER/db/Pfam-A.hmm.gz
 	gunzip $BEAV_DIR/software/TIGER/db/Pfam-A.hmm.gz
 else
-	echo -e "Pfam-A.hmm already exists"	
+	echo -e "Pfam-A.hmm already exists, doing nothing."	
 fi
 
 echo -e ""
+
 echo -e "DBSCAN-SWA"
 echo -e "DBSCAN-SWA will download a database (~600 Mb) on first run"
-
 echo -e ""
-#GapMind
+
 echo -e "GAPMIND"
 #make user install PaperBLAST in BEAV_DIR/software/
 #then run script to download and format dbs
 #gapmind aa carbon and format
 
 path_to_usearch=$(which usearch)
-#if [[ -f $BEAV_DIR/software/PaperBLAST/bin/usearch ]]; then
-#	echo -e "usearch is already added to the correct directory"
 if [[ ! -x $path_to_usearch ]]; then 
 	echo -e "Please download the usearch executable from https://www.drive5.com/usearch/download.html"
 	echo -e "Extract this file, rename it to usearch, make it executable (chmod +x usearch) and place it in a location in your PATH before running beav"
@@ -93,8 +102,8 @@ formatdb -p T -o T -i $BEAV_DIR/software/PaperBLAST/tmp/path.aa/curated.faa
 usearch -makeudb_ublast $BEAV_DIR/software/PaperBLAST/tmp/path.aa/curated.faa -output $BEAV_DIR/software/PaperBLAST/tmp/path.aa/curated.faa.udb
 formatdb -p T -o T -i $BEAV_DIR/software/PaperBLAST/tmp/path.aa/curated.faa
 
-
 echo -e ""
+
 echo -e "DONE"
 echo -e ""
 
