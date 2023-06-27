65ec3e74177270db0eb9faac75228ca4a4e180a3
Alexandra Weisberg <weisbeal@onid.oregonstate.edu>
2023-06-26 23:05:55 -0700

    gapmind fix

--- scripts/combine_annotations.py
+++ scripts/combine_annotations.py
@@ -128,9 +128,9 @@ for record in SeqIO.parse(f"./{strain}/{strain}.gbff","gb"):
 
             if locus_tags[0] in gapmind_dict:
                 if "note" in integron_gene:
-                    feature.qualifiers["note"].append("GapMind: " + gapmind_dict[locus_tags[0]])
+                    feature.qualifiers["note"].append(gapmind_dict[locus_tags[0]])
                 else:
-                    feature.qualifiers["note"] = "GapMind: " + gapmind_dict[locus_tags[0]]
+                    feature.qualifiers["note"] = gapmind_dict[locus_tags[0]]
 
 
     if record.id in hmmdb:
