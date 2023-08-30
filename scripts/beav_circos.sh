de6382de348854eb65e08657b252c3d862e81028
Arafat Rahman <arafat.rahman@email.ucr.edu>
2023-08-30 13:23:48 -0700

    Create beav_circos.sh

---
+++ scripts/beav_circos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+beav_dir=$1
+
+# Address to main beav gbk file
+GBK=`ls $1/*gbk`
+
+# Contig list
+CONTIG=`cat beav_RAU1/*oncogenic_plasmid_final.out.contiglist | cut -f 1 | head -n 1`
+
+# Conditionally run python script
+if [ -z $CONTIG ]
+then
+    python3 beav_circos.py $GBK
+else
+    python3 beav_circos.py $GBK $CONTIG
+fi
